459b98a0838d6651a872ad5f9c546d3f141e70eb
Luriahc <faqih.chairul08@gmail.com>
2025-12-31 13:19:29 +0800

    REMED SAD KETINGGALAN COMMIT

--- src/querry/remed/remed_sad.sql
+++ src/querry/remed/remed_sad.sql
@@ -2,132 +2,83 @@ DROP DATABASE IF EXISTS cleaning_system;
 CREATE DATABASE cleaning_system;
 USE cleaning_system;
 
-CREATE TABLE `comment` (
-  `id` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `id_gambar` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+Drop TABLE IF EXISTS like;
+Drop TABLE IF EXISTS comment;
+Drop TABLE IF EXISTS post;
+Drop TABLE IF EXISTS gambar;
+Drop TABLE IF EXISTS pengguna;  
 
--- --------------------------------------------------------
-
---
--- Table structure for table `gambar`
---
-
-CREATE TABLE `gambar` (
-  `id` int(11) NOT NULL,
-  `judul` varchar(50) NOT NULL,
-  `caption` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `like`
---
-
-CREATE TABLE `like` (
-  `id` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `id_gambar` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pengguna`
---
-
-CREATE TABLE `pengguna` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(20) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `post`
---
-
-CREATE TABLE `post` (
-  `id` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `id_gambar` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `comment`
---
-ALTER TABLE `comment`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_user_like` (`id_user`),
-  ADD KEY `fk_content_like` (`id_gambar`);
-
---
--- Indexes for table `gambar`
---
-ALTER TABLE `gambar`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `like`
---
-ALTER TABLE `like`
-  ADD KEY `fk_user_like` (`id_user`),
-  ADD KEY `fk_content_like` (`id_gambar`);
-
---
--- Indexes for table `pengguna`
---
-ALTER TABLE `pengguna`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `post`
---
-ALTER TABLE `post`
-  ADD KEY `fk_user_like` (`id_user`),
-  ADD KEY `fk_content_like` (`id_gambar`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `comment`
---
-ALTER TABLE `comment`
-  ADD CONSTRAINT `fk_gambar_comment` FOREIGN KEY (`id_gambar`) REFERENCES `gambar` (`id`),
-  ADD CONSTRAINT `fk_user_comment` FOREIGN KEY (`id_user`) REFERENCES `pengguna` (`id`);
-
---
--- Constraints for table `like`
---
-ALTER TABLE `like`
-  ADD CONSTRAINT `fk_content_like` FOREIGN KEY (`id_gambar`) REFERENCES `gambar` (`id`) ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_user_like` FOREIGN KEY (`id_user`) REFERENCES `pengguna` (`id`) ON UPDATE NO ACTION;
-
---
--- Constraints for table `post`
---
-ALTER TABLE `post`
-  ADD CONSTRAINT `fk_gambar_post` FOREIGN KEY (`id_gambar`) REFERENCES `gambar` (`id`),
-  ADD CONSTRAINT `fk_user_post` FOREIGN KEY (`id_user`) REFERENCES `pengguna` (`id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-CREATE TABLE log_hapus_gambar (
+CREATE TABLE pengguna (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    id_gambar INT,
-    judul VARCHAR(50),
-    deleted_at DATETIME
-);
+    nama VARCHAR(20) NOT NULL,
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE gambar (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    judul VARCHAR(50) NOT NULL,
+    caption TEXT NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE post (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_user INT NOT NULL,
+    id_gambar INT NOT NULL,
+
+    CONSTRAINT fk_user_post
+        FOREIGN KEY (id_user)
+        REFERENCES pengguna(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+
+    CONSTRAINT fk_gambar_post
+        FOREIGN KEY (id_gambar)
+        REFERENCES gambar(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE comment (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_user INT NOT NULL,
+    id_gambar INT NOT NULL,
+
+    CONSTRAINT fk_user_comment
+        FOREIGN KEY (id_user)
+        REFERENCES pengguna(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+
+    CONSTRAINT fk_gambar_comment
+        FOREIGN KEY (id_gambar)
+        REFERENCES gambar(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE like (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_user INT NOT NULL,
+    id_gambar INT NOT NULL,
+
+    CONSTRAINT fk_user_like
+        FOREIGN KEY (id_user)
+        REFERENCES pengguna(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+
+    CONSTRAINT fk_gambar_like
+        FOREIGN KEY (id_gambar)
+        REFERENCES gambar(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE INDEX idx_post_user ON post(id_user);
+CREATE INDEX idx_post_gambar ON post(id_gambar);
+CREATE INDEX idx_comment_user ON comment(id_user);
+CREATE INDEX idx_comment_gambar ON comment(id_gambar);
+CREATE INDEX idx_like_user ON like(id_user);
+CREATE INDEX idx_like_gambar ON like(id_gambar);
 COMMIT;
